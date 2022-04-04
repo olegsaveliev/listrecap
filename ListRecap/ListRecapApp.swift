97ce53d9e4dd3893211898f3aba9a8474eb71dd5
osavelyev <osavelyev@gmail.com>
2022-04-04 17:52:40 +0300

    Initial Commit

---
+++ ListRecap/ListRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListRecapApp.swift
+//  ListRecap
+//
+//  Created by Oleg Savelyev on 04.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ListRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
