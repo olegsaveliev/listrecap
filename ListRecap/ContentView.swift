12558e9fbcfb29bc9846ef36a04126bff0596979
osavelyev <osavelyev@gmail.com>
2022-04-04 18:13:49 +0300

    add List and Button

--- ListRecap/ContentView.swift
+++ ListRecap/ContentView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var array = ["one","two","three","four"]
+    @State var array1 = [String]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        VStack{
+            
+            
+            List(array1, id: \.self) { item in
+                Text(item)
+            }
+        
+            Button("ADD") {
+                var ranEl = Int.random(in: 0...array.count-1)
+                let new = array1.append(array[ranEl])
+                    
+                    }
+                
+                  }
+                    
+                }
+            }
+        
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
